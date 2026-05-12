7c161198821fd7c03015674f63455bfe3316b072
BeavyHombs <nikster731@gmail.com>
2026-05-12 16:21:06 -0700

    Starting Trie Stuff
    why does python make no sense compared to java oop??? (ik this is a bad take, okay?)

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # open the file 
 
+file = open(eng2000.txt)
+
 # sort the file into a trie 
 
+class Trie: 
+    def __init__(self):
+        pass
+
+class TrieNode:
+    def __init__(self):
+        pass
 # import game state (matrix?), can be hard coded for now 
 
 # implement algorithm, print possible words
